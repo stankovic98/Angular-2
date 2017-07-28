e6161515f34cfb296f17322ef9eb1af8a362657f
Antonio Stankovic <antoniostankovic123@gmail.com>
2017-07-28 12:41:50 +0200

    Postavljeni putevi za routing u app.module.ts

---
+++ app/products/product-deatil.component.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ProductDetail = (function () {
+    function ProductDetail() {
+    }
+    return ProductDetail;
+}());
+exports.ProductDetail = ProductDetail;
+//# sourceMappingURL=product-deatil.component.js.map
