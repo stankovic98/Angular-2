6423972a87be245a859cb68afff8e8eb427b9fb9
Antonio Stankovic <antoniostankovic123@gmail.com>
2017-07-28 13:59:22 +0200

    back button dodan

--- app/products/product-detail.component.js
+++ app/products/product-detail.component.js
@@ -12,13 +12,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var ProductDetailComponent = (function () {
-    function ProductDetailComponent(_route) {
+    function ProductDetailComponent(_route, _router) {
         this._route = _route;
+        this._router = _router;
         this.pageTitle = 'Product Detail';
     }
     ProductDetailComponent.prototype.ngOnInit = function () {
         var id = +this._route.snapshot.params['id'];
         this.pageTitle += ":" + id;
+    };
+    ProductDetailComponent.prototype.onBack = function () {
+        this._router.navigate(['/products']);
     };
     return ProductDetailComponent;
 }());
@@ -26,7 +30,7 @@ ProductDetailComponent = __decorate([
     core_1.Component({
         templateUrl: 'app/products/product-detail.component.html'
     }),
-    __metadata("design:paramtypes", [router_1.ActivatedRoute])
+    __metadata("design:paramtypes", [router_1.ActivatedRoute, router_1.Router])
 ], ProductDetailComponent);
 exports.ProductDetailComponent = ProductDetailComponent;
 //# sourceMappingURL=product-detail.component.js.map
